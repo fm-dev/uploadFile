d2610ac03f13306ee597f36e478f0f0e10df3f71
fm-dev <fm1441h@gmail.com>
2025-07-13 07:18:25 +0700

    checnk ip

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '103.56.148.116',
 	'username' => 'root',
 	'password' => 'fajri123#',
 	'database' => 'uploadfile',
